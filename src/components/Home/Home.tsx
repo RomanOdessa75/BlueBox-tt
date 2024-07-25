75c6daa4b58530249fa4d78172741939cd40aa40
RomanOdessa75 <115027650+RomanOdessa75@users.noreply.github.com>
2024-07-26 00:41:20 +0300

    main-add-home-component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,71 @@
-// import { useState } from 'react'
-// import { Navbar, Nav, Container, Image } from 'react-bootstrap'
-// import { Container } from 'react-bootstrap'
-// import { Link } from 'react-router-dom'
+import { Table, Container, Row, Col } from 'react-bootstrap'
 
-const Home: React.FC = () => {
-  return <div>Table</div>
+function Home() {
+  return (
+    <>
+      <Container fluid>
+        <Row>
+          <Col md="12">
+            <Table className="table-hover table-striped">
+              <thead>
+                <tr>
+                  <th className="border-0">ID</th>
+                  <th className="border-0">Name</th>
+                  <th className="border-0">Salary</th>
+                  <th className="border-0">Country</th>
+                  <th className="border-0">City</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Dakota Rice</td>
+                  <td>$36,738</td>
+                  <td>Niger</td>
+                  <td>Oud-Turnhout</td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Minerva Hooper</td>
+                  <td>$23,789</td>
+                  <td>Curaçao</td>
+                  <td>Sinaai-Waas</td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Sage Rodriguez</td>
+                  <td>$56,142</td>
+                  <td>Netherlands</td>
+                  <td>Baileux</td>
+                </tr>
+                <tr>
+                  <td>4</td>
+                  <td>Philip Chaney</td>
+                  <td>$38,735</td>
+                  <td>Korea, South</td>
+                  <td>Overland Park</td>
+                </tr>
+                <tr>
+                  <td>5</td>
+                  <td>Doris Greene</td>
+                  <td>$63,542</td>
+                  <td>Malawi</td>
+                  <td>Feldkirchen in Kärnten</td>
+                </tr>
+                <tr>
+                  <td>6</td>
+                  <td>Mason Porter</td>
+                  <td>$78,615</td>
+                  <td>Chile</td>
+                  <td>Gloucester</td>
+                </tr>
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
 }
 
 export default Home
